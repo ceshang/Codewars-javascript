c43eebb7195d02e2cb16ea48f4f52b9bfb2563c0
ceshang <shangce919@hotmail.com>
2016-01-13 16:48:31 -0600

    Update sum_of_pairs.js
    add instructions

--- 5/sum_of_pairs.js
+++ 5/sum_of_pairs.js
@@ -1,3 +1,14 @@
+/*
+Given a list of integers and a single sum value, 
+return the first two values (parse from the left please) in order of appearance that add up to form the sum.
+sum_pairs([11, 3, 7, 5], 10)== [3, 7]
+
+sum_pairs([4, 3, 2, 3, 4],6)== [4, 2]
+
+sum_pairs([0, 0, -2, 3], 2)== None/nil/undefined (Based on the language)
+
+sum_pairs([10, 5, 2, 3, 7, 5],10)== [3, 7]
+*/
 
 var sum_pairs=function(ints, s){
     var pair = new Array();
