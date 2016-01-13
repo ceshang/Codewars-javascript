63aa868ef9aecea34bc217af44aca80b5152494a
ceshang <shangce919@hotmail.com>
2016-01-13 16:43:16 -0600

    Update calculating_with_functions.js
    add instruction

--- 5/calculating_with_functions.js
+++ 5/calculating_with_functions.js
@@ -1,3 +1,12 @@
+
+/*
+This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+  seven(times(five())); // must return 35
+  four(plus(nine())); // must return 13
+  eight(minus(three())); // must return 5
+  six(dividedBy(two())); // must return 3
+*/
+
 var cal = function(num,func){
   return func==undefined? num:func(num);
 }
