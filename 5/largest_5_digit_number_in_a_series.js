88aa72c1aee68d2dda0b0d470ac9582f16783740
ceshang <shangce919@hotmail.com>
2016-02-23 20:17:00 -0600

    Largest 5 digit number in a series

---
+++ 5/largest_5_digit_number_in_a_series.js
@@ -0,0 +1,14 @@
+/*
+In the following 6 digit number:
+
+283910
+91 is the greatest sequence of 2 digits.
+
+Complete the solution so that it returns the largest five digit number found within the number given.. 
+The number will be passed in as a string of only digits. It should return a five digit integer. 
+The number passed may be as large as 1000 digits.
+*/
+function solution(digits){
+  if (digits.length <= 5) return Number(digits);
+  return Math.max(Number(digits.substr(0, 5)), solution(digits.substr(1)));
+}
