d96d087f56489580c1d1a3ce6e56f5075704fe6d
ceshang <shangce919@hotmail.com>
2016-03-28 15:51:44 -0500

    Sum strings as numbers

--- 4/sum_strings_as_numbers.js
+++ 4/sum_strings_as_numbers.js
@@ -1,10 +1,10 @@
 
 /*
-This time we want to write calculations using functions and get the results. Let's have a look at some examples:
-  seven(times(five())); // must return 35
-  four(plus(nine())); // must return 13
-  eight(minus(three())); // must return 5
-  six(dividedBy(two())); // must return 3
+Given the string representations of two integers, return the string representation of the sum of those integers.
+
+For example:
+
+sumStrings('1','2') // => '3'
 */
 
 function sumStrings(a,b) {
