b3c665dd9f8f3589d3e577ae438dec31d1cbb843
ceshang <shangce919@hotmail.com>
2016-04-22 23:13:03 -0500

    Rotate Array

---
+++ 6/rotate_array.js
@@ -0,0 +1,29 @@
+/*
+Create a function named "rotate" that takes an array and returns a new one with the elements inside rotated n spaces.
+
+If n is greater than 0 it should rotate the array to the right. If n is less than 0 it should rotate the array to the left. If n is 0, then it should return the array unchanged.
+
+Example:
+
+var data = [1, 2, 3, 4, 5];
+
+rotate(data, 1) // => [5, 1, 2, 3, 4]
+rotate(data, 2) // => [4, 5, 1, 2, 3]
+rotate(data, 3) // => [3, 4, 5, 1, 2]
+rotate(data, 4) // => [2, 3, 4, 5, 1]
+rotate(data, 5) // => [1, 2, 3, 4, 5]
+
+rotate(data, 0) // => [1, 2, 3, 4, 5]
+
+rotate(data, -1) // => [2, 3, 4, 5, 1]
+rotate(data, -2) // => [3, 4, 5, 1, 2]
+rotate(data, -3) // => [4, 5, 1, 2, 3]
+rotate(data, -4) // => [5, 1, 2, 3, 4]
+rotate(data, -5) // => [1, 2, 3, 4, 5]
+
+*/
+
+function rotate(array, n) {
+  n = n % array.length;
+  return array.slice(-n).concat(array.slice(0, -n));
+}
